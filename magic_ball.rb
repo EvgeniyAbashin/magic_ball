14d4798443968f06e999c33ebb968b1b7fb5a055
EvgeniyAbashin <39528278+EvgeniyAbashin@users.noreply.github.com>
2019-01-07 14:02:51 +0300

    Update magic_ball.rb

--- magic_ball.rb
+++ magic_ball.rb
@@ -5,28 +5,28 @@ hello = [
 ]
 
 answers = [
-# Положительные
+  # Положительные
   "It is certain (Бесспорно)",
   "It is decidedly so (Предрешено)",
   "Without a doubt (Никаких сомнений)",
   "Yes — definitely (Определённо да)",
   "You may rely on it (Можешь быть уверен в этом)",
     
-# Нерешительно положительные
+  # Нерешительно положительные
   "As I see it, yes (Мне кажется — «да»)",
   "Most likely (Вероятнее всего)",
   "Outlook good (Хорошие перспективы)",
   "Signs point to yes (Знаки говорят — «да»)",
   "Yes (Да)",
 
-# Нейтральные
+  # Нейтральные
   "Reply hazy, try again (Пока не ясно, попробуй снова)",
   "Ask again later (Спроси позже)",
   "Better not tell you now (Лучше не рассказывать)",
   "Cannot predict now (Сейчас нельзя предсказать)",
   "Concentrate and ask again (Сконцентрируйся и спроси опять)",
 
-# Отрицательные
+  # Отрицательные
   "Don’t count on it (Даже не думай)",
   "My reply is no (Мой ответ — «нет»)",
   "Outlook not so good (Перспективы не очень хорошие)",
